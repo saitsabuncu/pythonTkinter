590fcb8f6af385019651dfb62bd7dab4defc58dc
Mustafa Sait Sabuncu <sabuncumustafasait@gmail.com>
2024-12-20 23:15:13 +0300

    tkinter egzersiz 2

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ window=t.Tk()
 window.title("Python Tkinter")
 window.minsize(width=450,height=300)
 
+#Label
+my_label=t.Label(text="this is a label",font=('Arial',30,'normal'))
+my_label.config(bg='black',fg='white')
+my_label.pack()
+
 window.mainloop()
