f8a3423da546275655e1baea87a4927496bdb61c
Mustafa Sait Sabuncu <sabuncumustafasait@gmail.com>
2024-12-21 08:29:49 +0300

    tkinter egzersiz

--- main.py
+++ main.py
@@ -4,5 +4,30 @@ import tkinter as t
 window=t.Tk()
 window.title("Python Tkinter")
 window.minsize(width=450,height=300)
+def click_button():
+    user_input = my_entry.get()
+    print(user_input)
+
+#Label
+my_label=t.Label(text="this is a label",font=('Arial',30,'normal'))
+my_label.config(bg='black',fg='white')
+my_label.pack()
+#my_label.pack(side='top')
+my_label.place(x=0, y=0)
+
+#button
+my_button=t.Button(text='this is a button', command=click_button)
+my_button.pack()
+#my_button.pack()
+my_button.place(x=225-63, y=150-14)
+my_button.update()
+print(my_button.winfo_height())
+print(my_button.winfo_width())
+
+#entry
+my_entry=t.Entry(width=20)
+my_entry.pack()
+#my_entry.pack()
+my_entry.place(x=300,y=200)
 
 window.mainloop()
